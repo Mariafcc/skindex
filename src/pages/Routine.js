d5939add7cf7c4b0f1eb51129e475cf676cf24d7
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-11-20 19:53:05 -0600

    added components to routine.js

--- src/pages/Routine.js
+++ src/pages/Routine.js
@@ -1,3 +1,47 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const Routine = () => {
+    return (
+        <div class="container">
+            <header>
+                <NavBar />
+                <p>
+                    <label for="fname">Product you have:</label>
+                    <input type="text" id="fname" name="fname"></input>
+                    <label for="cars">Product type:</label>
+                    <select name="product" id="products">
+                        <option value="cleanser">Cleanser</option>
+                        <option value="toner">Toner</option>
+                        <option value="essence">Essence</option>
+                        <option value="serum">Serum</option>
+                        <option value="moisturizer">Moisturizer</option>
+                        <option value="eye">Eye Cream</option>
+                        <option value="exfoliator">Exfoliator</option>
+                        <option value="mask">Mask</option>
+                        <option value="spf">SPF</option>
+                    </select>
+                    <input type="submit" value="Submit"></input>
+                </p>
+            </header>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        1 of 2
+                    </div>
+                    <div class="col">
+                        2 of 2
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Routine;
+
 //nav bar at the top
 //User will have already taken a quiz to show recommendations 
 //recommendations should be displayed after the quiz and when an item is deleted 
