bfbd056bff7bd8754b82f5f1d75bc156aa87f457
Maria Cruz <66399861+Mariafcc@users.noreply.github.com>
2020-11-22 12:22:29 -0600

    Mc9 (#12)
    * routine page changes in css and layout
    
    * added handlers to quiz and product selector
    
    Co-authored-by: Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
    Co-authored-by: Caroline Strickland <carolinelstrickland@outlook.com>

--- src/components/ProductSelector.js
+++ src/components/ProductSelector.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./style/productSelector.css"
+import { useState } from "react";
+
 
 
 const ProductSelector = () => {
+    const [product, setProduct] = useState();
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log("product input " + product);
+        
+    };
+
+
+
     return (
         <div className="container">
             <img src="./img/routine.jpg" class="img-fluid" alt="Responsive image"></img>
+            <div className="centered">
+                <label className="pname" for="fname">Product you have:</label>
+                <input className="pinput" type="text" type="text"
+                    placeholder="Product you have"
+                    name="product" onChange={e => setProduct(e.target.value)}></input>
+                <br></br>
+                <label className="ptype" for="type">Product type:</label>
             <div class="centered">
                 <label className="pname" for="fname">Product you have:</label>
                 <input className="pinput" type="text" id="fname" name="fname"></input>
@@ -22,6 +41,13 @@ const ProductSelector = () => {
                     <option value="mask">Mask</option>
                     <option value="spf">SPF</option>
                 </select>
+                <button type="button" className="selectorBtn"
+                    onClick={handleSubmit}>Submit</button>
+                {/* <div>
+                    <p>{product}</p>
+                </div> */}
+            </div>
+
                 <button type="button" className="selectorBtn" >Submit</button>
             </div>
         </div>
