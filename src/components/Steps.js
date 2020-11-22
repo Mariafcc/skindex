c399afd268c948be5332d4e371f7bd299eb74db7
Caroline Strickland <carolinelstrickland@outlook.com>
2020-11-22 13:04:45 -0600

    Revert "Mc9 (#12)"
    This reverts commit bfbd056bff7bd8754b82f5f1d75bc156aa87f457.

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,23 +1,15 @@
 import React from "react";
 import "./style/steps.css";
-import ProductSelector from "./ProductSelector"
+
 
 const Steps = () => {
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-8">This will display the google map and recommendations</div>
-                <div class="col-4">This is your routine!
-                {/* <div>
-                    <p>{product}</p>
-                </div> */}
-                </div>
-            </div>
-        </div>
-
         <div className="container">
             <img className="img-fluid" src="/img/routineList.jpg" alt="serum" />
-        </div>        
+            
+
+        </div>
+        
     )
 };
 
