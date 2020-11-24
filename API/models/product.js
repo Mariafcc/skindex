3aa3523b18a9ebdb5bfa9f6d77bdbd1401b3539b
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-11-23 23:37:25 -0600

    starter database

--- API/models/product.js
+++ API/models/product.js
@@ -1,15 +1,31 @@
-// module.exports = (sequelize, Sequelize) => {
-// 	const Product = sequelize.define("product", {
-// 		name: {
-// 			type: Sequelize.STRING
-// 		},
-// 		email: {
-// 			type: Sequelize.STRING
-// 		},
-// 		password: {
-// 			type: Sequelize.STRING
-// 		}
-// 	});
+module.exports = (sequelize, Sequelize) => {
+    const Product = sequelize.define("product", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        brand: {
+            type: Sequelize.STRING
+        },
+        name: {
+            type: Sequelize.STRING
+        },
+        type: {
+            type: Sequelize.STRING
+        },
+        store: {
+            type: Sequelize.STRING
+        },
+        skinType: {
+            type: Sequelize.STRING
+        },
+        price: {
+            type: Sequelize.STRING
+        },
+        goal: {
+            type: Sequelize.STRING
+        }
+    });
 
-// 	return User;
-// };
+    return Product;
+};
