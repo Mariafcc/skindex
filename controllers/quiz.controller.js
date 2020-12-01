116cf095c798c95ea3d880ee566bc660d8b8d91c
Caroline Strickland <carolinelstrickland@outlook.com>
2020-12-01 11:47:02 -0600

    Lucy ten (#42)
    * omg
    
    * creating controller for recommendations
    
    * creating controllers for recommendations
    
    * adding readme.md

--- controllers/quiz.controller.js
+++ controllers/quiz.controller.js
@@ -1,9 +1,17 @@
 const db = require("../models");
+<<<<<<< HEAD
+//const Tutorial = db.tutorials;
+=======
+>>>>>>> 99520f8160172e6280c05db847f6c6dfd438aad0
 const Op = db.Sequelize.Op;
 const Results = db.results;
 
 
+<<<<<<< HEAD
+exports.getResultss = (req, res) => {
+=======
 exports.getResults = (req, res) => {
+>>>>>>> 99520f8160172e6280c05db847f6c6dfd438aad0
     Results.findAll()
     .then(results => {
         res.status(200).send({
