387e5a3520fd522a6a2827c8ad5a2a757e0bee0b
Caroline Strickland <carolinelstrickland@outlook.com>
2020-12-01 16:21:18 -0600

    Lucy eleven (#43)
    * omg
    
    * creating controller for recommendations
    
    * creating controllers for recommendations
    
    * adding readme.md
    
    * about blurb for landing page
    
    Co-authored-by: Maria Cruz <66399861+Mariafcc@users.noreply.github.com>

--- client/src/components/DescriptionCard.js
+++ client/src/components/DescriptionCard.js
@@ -6,8 +6,9 @@ function DescriptionCard() {
     return (
         <div>
             <Card className="opacity-4 bg-light">
-                <Card.Body className="text-black opacity">TCats woo love me! meeeeouw. Cry louder at reflection climb into cupboard and lick the salt off rice cakes chirp at birds i hate cucumber pls dont throw it at me but bird bird bird bird bird bird human why take bird out i could have eaten that but head nudges.</Card.Body>
-                <Card.Body>Cats are the world fall asleep on the washing machine purr when give birth for take a deep sniff of sock then walk around with mouth half open there's a forty year old lady there let us feast, or i love cats i am one wake up scratch humans leg for food then purr then i have a and relax asdflkjaertvlkjasntvkjn (sits on keyboard).</Card.Body>
+                <Card.Body className="text-black opacity" >
+                    SkinDex is an application made just for you. Beginning your SkinDex experience with our skin care assessment, we can build a personalized skincare routine that meets your specific needs based on your skin type, primary skin concerns, budget and type of routine you are looking for. With our handpicked list of recommended products, we are able to provide you with an easy way to start and end your day and update your routine as your needs change. 
+                </Card.Body>
             </Card>
         </div>
     )
