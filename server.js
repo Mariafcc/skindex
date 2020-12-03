a5244a6ef6901dc75b7544268a0fdca2202f2bbf
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-12-02 21:47:38 -0600

    heroku try 6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path')
 const fs = require('fs');
 
 const app = express();
@@ -8,13 +9,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
+app.use(express.static('client/build'));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
+
+if(process.env.NODE_ENV === 'production') {  app.use(express.static(path.join(__dirname, 'client/build')));   
+app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+
+
+// app.get('*', (request, response) => {
+// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 //run initially
 // const seeds = require("./seeds");
 // db.sequelize.sync({force: true}).then(() => {
