c534b5639cc5f686c5918424e573d5f9ea7e2110
Maria Cruz <66399861+Mariafcc@users.noreply.github.com>
2020-12-04 23:08:55 -0600

    trying again (#72)
    Co-authored-by: Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>

--- routes/zedd.routes.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = function (app) {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname = 'client/build/index.html'));
-    });
-}
-
-//to run locally switch build to client 
