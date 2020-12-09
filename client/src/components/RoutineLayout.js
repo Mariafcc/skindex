a022257d8ef9375410810f26cd67b92b06cfeb10
Mari <marinarosemartin@gmail.com>
2020-12-08 20:43:19 -0600

    login typo

--- client/src/components/RoutineLayout.js
+++ client/src/components/RoutineLayout.js
@@ -25,7 +25,7 @@ const RoutineLayout = () => {
         }} >
             <Container>
                 <Row>
-                    <Col className="col-sm-6 col-md-6 col-lg-12 center-text">
+                    <Col className="col-sm-6 col-md-12 col-lg-12 center-text">
                         <ProductSelector />
                         <Button onClick={handleShow} className="m-4">Find Store</Button>
                         <Modal show={show} onHide={handleClose} >
