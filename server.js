4ad91f06fd2ca0cce988a830ad98ecff7ab44e15
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-12-10 21:27:04 -0600

    heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
-app.use(express.static('client/public'));
+// app.use(express.static('client/public'));
 
 
 
