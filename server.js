cc1b0b04f0fff63d3977b76a71445e85bd438e54
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-12-10 20:47:11 -0600

    chchchaa changes

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.static('client/public'));
 
 
 //run initially
-const seeds = require("./seeds");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log('Drop and Resync Db');
-    seeds.productSeeds();
-});
+// const seeds = require("./seeds");
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log('Drop and Resync Db');
+//     seeds.productSeeds();
+// });
 
 //comment out initially
-// db.sequelize.sync();
+db.sequelize.sync();
 
 // load in routes
 // fs.readdir('./routes', (err, files) => {
