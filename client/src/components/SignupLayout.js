c21f6ff6ec52735712d6aa19b1e3c360d3bc089b
Maria Cruz <mariafcc01@icloud.com>
2020-12-14 12:45:47 -0600

    Mc83 (#131)
    * trying to disable register
    
    * heroku changes taking out spinner. sorrryyy.
    
    Co-authored-by: Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>

--- client/src/components/SignupLayout.js
+++ client/src/components/SignupLayout.js
@@ -69,7 +69,7 @@ const SignupLayout = () => {
     const [message, setMessage] = useState("");
     const history = useHistory();
 
-    const [showSpinner, setShowSpinner] = useState(false);
+    // const [showSpinner, setShowSpinner] = useState(false);
 
     const onChangeUsername = (e) => {
         const username = e.target.value;
@@ -91,7 +91,7 @@ const SignupLayout = () => {
 
         setMessage("");
         setSuccessful(false);
-        setShowSpinner(true);
+        // setShowSpinner(true);
 
         form.current.validateAll();
 
@@ -123,14 +123,16 @@ const SignupLayout = () => {
 
     useEffect(() => {
         if (successful) {
-            setShowSpinner(false);
+            // setShowSpinner(false);
             redirectAfterSuccessfulRegister();
         }
     }, [successful]);
 
+
+
     return (
         <div>
-            {showSpinner ? <Spinner visible={showSpinner} /> :
+            {/* {showSpinner ? <Spinner visible={showSpinner} /> : */}
                 <Form onSubmit={handleRegister} ref={form}>
                     <h3>Signup for Routine</h3>
 
@@ -158,7 +160,7 @@ const SignupLayout = () => {
                             validations={[required, vPassword]} className="form-control" placeholder="Enter password" />
                     </div>
 
-                    <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                    <button type="submit" disabled={!password} className="btn btn-dark btn-lg btn-block">Register</button>
                     {message && (
                         <div className="form-group">
                             <div
@@ -172,7 +174,7 @@ const SignupLayout = () => {
                     )}
                     <CheckButton style={{ display: "none" }} ref={checkBtn} />
                 </Form>
-            }
+            {/* } */}
         </div>
     );
 
--- config/db.config.js
+++ config/db.config.js
@@ -1,27 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "maria",
-    PASSWORD: "maria",
-    DB: "productlist",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+	HOST: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	USER: "hro7a2y297vy6173",
+	PASSWORD: "dqnqx1b2fgkkw2s6",
+	DB: "pcl2pvbuikbgjtk4",
+	dialect: "mysql",
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
 };
-
-// module.exports = {
-// 	HOST: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-// 	USER: "hro7a2y297vy6173",
-// 	PASSWORD: "dqnqx1b2fgkkw2s6",
-// 	DB: "pcl2pvbuikbgjtk4",
-// 	dialect: "mysql",
-// 	pool: {
-// 		max: 5,
-// 		min: 0,
-// 		acquire: 30000,
-// 		idle: 10000
-// 	}
-// };
